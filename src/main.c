fd1fea4598b88deadac95395de083a5b28f8bbd8
42ghan <ghan@student.42seoul.kr>
2021-09-10 11:57:43 +0900

    m_feat : handle SIGINT & SIGQUIT like bash

--- src/main.c
+++ src/main.c
@@ -12,6 +12,19 @@
 
 #include "../include/minishell.h"
 
+void	signal_handler(int signal)
+{
+	if (signal == SIGINT)
+	{
+		write(STDOUT_FILENO, "\n", 1);
+		rl_on_new_line();
+		rl_replace_line("", 0);
+	}
+	else if (signal == SIGQUIT)
+		rl_on_new_line();
+	rl_redisplay();
+}
+
 int	main(int argc, char *argv[], char *envp[])
 {
 	char	*line_read;
@@ -19,6 +32,8 @@ int	main(int argc, char *argv[], char *envp[])
 	envp = 0;
 	if (argc > 1 || argv[1])
 		is_error(NULL, NULL, "esh does not receive arguments", EXIT_FAILURE);
+	signal(SIGINT, signal_handler);
+	signal(SIGQUIT, signal_handler);
 	while (1)
 	{
 		line_read = readline("🐘 esh > ");
