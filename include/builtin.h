e2289bd32455203110fa4f8196df87303db09559
yongjulejule <lyjshow200@gmail.com>
2021-09-24 13:58:07 +0900

    m_fix : fix export func prototype

--- include/builtin.h
+++ include/builtin.h
@@ -6,7 +6,7 @@
 /*   By: yongjule <yongjule@student.42seoul.kr      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/23 10:31:34 by yongjule          #+#    #+#             */
-/*   Updated: 2021/09/24 13:24:49 by yongjule         ###   ########.fr       */
+/*   Updated: 2021/09/24 13:56:16 by yongjule         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ typedef enum e_builtin
 int	is_error_no_exit(char *str1, char *str2, char *err_msg, int status);
 
 /*Builtin Commands*/
-void	exprot(const char *path, char *const argv[], char **envp);
+void	exprot(const char *path, char *const argv[], char *const envp[]);
 void	echo(const char *path, char *const argv[], char *const envp[]);
 void	ext(const char *path, char *const argv[], char *const envp[]);
 void	cd(const char *path, char *const argv[], char *const envp[]);
