75c355bd191bb88960a2e1d2378bc74f17d99db2
Gyoungmin Han <ghan@c8r4s8.42seoul.kr>
2021-09-27 11:28:28 +0900

    m_fix : env export unset fix

--- src/builtin/handle_error.c
+++ src/builtin/handle_error.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   handle_error.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ghan <ghan@student.42seoul.kr>             +#+  +:+       +#+        */
+/*   By: ghan <ghan@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 11:26:44 by yongjule          #+#    #+#             */
-/*   Updated: 2021/09/27 02:12:05 by ghan             ###   ########.fr       */
+/*   Updated: 2021/09/27 11:27:51 by ghan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 int	is_error_no_exit(char *str1, char *str2, char *err_msg, int status)
 {
 	ft_putstr_fd("🤣 esh: ", STDERR_FILENO);
-	if (!str1)
+	if (str1)
 		ft_putstr_fd(str1, STDERR_FILENO);
-	if (!str2)
+	if (str2)
 		ft_putstr_fd(str2, STDERR_FILENO);
-	if (!err_msg)
+	if (err_msg)
 		ft_putstr_fd(err_msg, STDERR_FILENO);
 	write(STDERR_FILENO, "\n", 1);
 	return (status);
