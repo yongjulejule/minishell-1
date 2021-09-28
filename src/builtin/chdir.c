67452ebb0085948a29cd3f134e0ca167a2810c1f
42ghan <ghan@student.42seoul.kr>
2021-09-28 11:54:31 +0900

    m_fix: remove unnecessary lines

--- src/builtin/chdir.c
+++ src/builtin/chdir.c
@@ -6,7 +6,7 @@
 /*   By: ghan <ghan@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 10:43:34 by yongjule          #+#    #+#             */
-/*   Updated: 2021/09/28 10:47:11 by ghan             ###   ########.fr       */
+/*   Updated: 2021/09/28 11:52:57 by ghan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,17 @@ static void	update_pwd(char ***envp)
 {
 	char	*cwd;
 	char	*argv_p[3];
-	char	*argv_op[3];
 
-	argv_op[0] = "export";
-	argv_op[1] = ft_strjoin("OLDPWD=", ft_get_envp(*envp, "PWD"));
-	argv_op[2] = NULL;
-	exprt("export", argv_op, envp);
-	cwd = getcwd(NULL, 0);
 	argv_p[0] = "export";
-	argv_p[1] = ft_strjoin("PWD=", cwd);
+	argv_p[1] = ft_strjoin("OLDPWD=", ft_get_envp(*envp, "PWD"));
 	argv_p[2] = NULL;
 	exprt("export", argv_p, envp);
-	free(cwd);
 	free(argv_p[1]);
-	free(argv_op[1]);
+	cwd = getcwd(NULL, 0);
+	argv_p[1] = ft_strjoin("PWD=", cwd);
+	exprt("export", argv_p, envp);
+	free(argv_p[1]);
+	free(cwd);
 }
 
 static void	cd_check_error(char *cmd, char *arg, char ***envp)
