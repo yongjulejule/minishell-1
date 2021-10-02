118322efead5518b435ea4f8989bf9868057aaec
yongjulejule <lyjshow200@gmail.com>
2021-10-02 09:36:35 +0900

    m_fix : fix complicited files

---
+++ src/builtin/exit.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ghan <ghan@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/23 12:01:16 by yongjule          #+#    #+#             */
+/*   Updated: 2021/10/02 09:35:35 by yongjule         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "builtin.h"
+
+static int	check_exit_arg_validity(char **argv)
+{
+	int	idx;
+	int	status;
+	int	sign;
+
+	idx = 0;
+	sign = 0;
+	if (!is_charset(argv[1][idx], "+-"))
+	{
+		idx++;
+		sign = 1;
+	}
+	while (ft_isdigit(argv[1][idx]))
+		idx++;
+	if ((idx > 19 + sign) || argv[1][idx] != '\0')
+	{
+		ft_putstr_fd("exit: ", STDERR_FILENO);
+		is_error(argv[1], ": ", "numeric argument required", 255);
+	}
+	status = ft_atoi(argv[1]);
+	if (ft_strlen(argv[1]) == (19 + sign) && (status == 0 || status == -1))
+	{
+		ft_putstr_fd("exit: ", STDERR_FILENO);
+		is_error(argv[1], ": ", "numeric argument required", 255);
+	}
+	return (status);
+
+}
+
+int	ext(const char *path, char *const argv[], char *const envp[])
+{
+	extern int	g_exit_code;
+
+	if (!path || !argv || !envp)
+	{
+		g_exit_code = is_error_no_exit("export : ", NULL,
+				"pass valid args to builtin functions", EXIT_FAILURE);
+		return (g_exit_code);
+	}
+	if (!argv[1])
+	{
+		ft_putendl_fd("exit", STDERR_FILENO);
+		exit(g_exit_code);
+	}
+	if (ft_strsetlen((char **)argv) > 2)
+	{
+		ft_putendl_fd("exit\n 🤣 esh: exit : too many arguments", STDERR_FILENO);
+		g_exit_code = 1;
+		return (-1);
+	}
+	exit(check_exit_arg_validity((char **)argv) & (0xff));
+}
---
+++ src/main.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ghan <ghan@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/09 12:08:34 by ghan              #+#    #+#             */
+/*   Updated: 2021/10/02 09:35:47 by yongjule         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	g_exit_code = 0;
+
+int	main(int argc, char *argv[], char *envp[])
+{
+	char	*line_read;
+	char	**ft_envp;
+	t_cmds	*cmdlst;
+
+	ft_envp = esh_pre_process(argc, argv, envp);
+	while (1)
+	{
+		unexp_eof_sig_handler();
+		line_read = readline("🐘 esh > ");
+		eof_exit(line_read);
+		add_history(rl_line_buffer);
+		parse_line_main(&cmdlst, line_read, ft_envp, ft_strdup(""));
+		if (cmdlst && cmdlst->cmd && *cmdlst->cmd)
+			exec_cmd_main(cmdlst, &ft_envp);
+		sigint_n_sigquit_handler(main_sig_handler);
+		free_cmds_lst(&cmdlst);
+		free(line_read);
+	}
+	return (EXIT_SUCCESS);
+}
